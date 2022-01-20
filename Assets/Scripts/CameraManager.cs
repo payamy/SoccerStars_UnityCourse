c9a94acc5cccb9c92fa19b2287f6f2f6c4943ac7
Payam Yarandi <payamyarandi98@gmail.com>
2022-01-20 16:17:48 +0330

    add CameraManager

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public float groundSize;
+
+    private void Start()
+    {
+        //const float targetAspect = 16.0f / 9.0f;
+        var windowAspect = Screen.width / (float)Screen.height;
+        var mainCamera = Camera.main;
+        if (mainCamera == null) return;
+        var size = mainCamera.orthographicSize;
+        //if (!(windowAspect < targetAspect)) return;
+        var size1 = groundSize / windowAspect;
+        mainCamera.orthographicSize = size1;
+    }
+}
